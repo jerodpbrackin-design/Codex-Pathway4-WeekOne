f7f0e6d98164e694e26dace92ce94505df7d32b8
jerodpbrackin-design <jerod.p.brackin@gmail.com>
2026-04-09 16:14:11 -0500

    Seats.py classroom prog

---
+++ seats.py
@@ -0,0 +1,10 @@
+def seats():
+    classRoom = ["Maria", "Joon", "Luis", "Ana"]
+    print(classRoom)
+    classRoom[0] = "Eder"
+    print(classRoom)
+    classRoom[2] = "Pedro"
+    print(classRoom)
+    return
+
+seats()
