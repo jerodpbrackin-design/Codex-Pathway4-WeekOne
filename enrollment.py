47ce18711d05d8b8a80f3faca14a57db7805e8a5
jerodpbrackin-design <jerod.p.brackin@gmail.com>
2026-04-10 14:33:05 -0500

    add typing to is_active in enrollment.py

--- enrollment.py
+++ enrollment.py
@@ -5,7 +5,7 @@ def enrollment():
     tuition_balance = float(input("Enter tuition balance: "))
     
     is_active_input = input("Is the student active (true/false): ").lower()
-    is_active = True if is_active_input == "true" else False
+    is_active:bool = True if is_active_input == "true" else False
 
     print("\n===== Student Enrollment Report =====")
     print(f"Name: {name} {last_name}")
